80a23ae36df39246b85dd9e56802bcdcd5d19669
Sam <165819040+smmartees@users.noreply.github.com>
2024-10-16 13:40:56 -0400

    Commit #1 - Unit Test set up. Projects connected. Existing files added. extern "C".

---
+++ BCSRec_UnitTests/BCSRec_UnitTests.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" {
+#include "../BCSRec/main.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BCSRecUnitTests
+{
+	TEST_CLASS(BCSRecUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+		TEST_METHOD(TestMethod2)
+		{
+		}
+		TEST_METHOD(TestMethod3)
+		{
+		}
+	};
+}
